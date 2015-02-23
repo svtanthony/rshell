4a7869f3265c662d8c6517a40fb6d343ab59a1d5
Roberto Pasillas <rpasi001@ucr.edu>
2015-02-23 08:22:05 -0800

    fixed most bugs

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -6,8 +6,11 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <sstream>
 #include <string>
+#include <vector>
+#include <fcntl.h>
 
 int numArgs = 50;
 int numLen = 50;
@@ -19,22 +22,53 @@ string terminalPrefix();
 void parse(string s, bool &run);
 int sysCalls();
 void init();
+int sysCallsLimited(int fd[], int fd2, int instruction, char *argv[]);
 void clear();
 void nukem();
 queue<string> split(string s);
 void itemCount(string s);
+
+void commands(vector<string> &token, bool &run);
+void doPipe(vector<string> &token, bool &run, int index);
+void doOr(vector<string> &token, bool &run, int index);
+void doAnd(vector<string> &token, bool &run, int index); 
+void doSemiColon(vector<string> &token, bool &run, int index);
+int redirectAndRun(vector<string> &token, bool &run, int index);
+int nextConnector(vector<string> &token);
 
 int main()
 {
    	string s,signature = terminalPrefix();// obtain username and hostname
 	bool run = 1;
 	
-	while(run)
+	while(run && cin)
 	{
 		cout.flush();
 		cout << signature << " $ ";
 		getline(cin, s);
-		parse(s,run);
+		//parse(s,run);
+		queue<string> list = split(s);
+		
+		vector<string> token;
+		while(!list.empty())
+		{
+			token.push_back(list.front());
+			list.pop();
+		}
+		
+		int childPID = fork();
+		if(childPID == -1) {
+			perror("fork error");
+			run = false;
+		}
+		if(childPID != 0) {
+			int status = 0;
+			waitpid(childPID, &status, 0);
+		} else {
+			run = 0;
+			commands(token, run);
+			exit(0);
+		}
 	}
 	return 0;
 }
@@ -81,20 +115,346 @@ int sysCalls()
 	return status;
 }
 
+int sysCallsLimited(int fd[], int fd2[], int instruction, char *argv[])/////////////////////////////////////////////////////
+{
+	//instruction 0 = close(1), dup(fd[0])
+	//instruction 1 = close(0), dup(fd[1]), close(1), dup(fd2[0])
+	//instruction 2 = close(0), dup(fd2[1])
+
+	if(instruction == 1)
+	{
+		close(0);
+		dup(fd[1]);//close fd[1]?
+		close(1);
+		dup(fd2[0]);//close fd2[0]?
+	}
+	else if(instruction == 0)
+	{
+		close(1);
+		dup(fd[0]);
+	}
+	else 
+		if(instruction == 2)
+		{
+			close(0);
+			dup(fd2[1]);
+		}
+
+	int pid;
+	if ((pid=fork())== -1) // fork error
+	{
+        perror("fork");
+		nukem();
+		exit(1);
+	}
+	else if (pid == 0) //child - run process and error check
+	{
+		if (execvp(argv[0],argv) != 0)
+		{
+			perror(argv[0]);
+			exit(1);
+		}
+	}else //parent
+		return pid;
+}
+
+void redirect(vector<string> &vExtracted)
+{
+	int redirections = 0;
+	for(int i(0); 1 < vExtracted.size(); i++)
+		if(vExtracted[i].compare("|") == 0 || vExtracted[i].find(">") != string::npos || vExtracted[i].find("<") != string::npos)
+			redirections++;
+	//**********************************************************************************************************	
+}
+
+void commands(vector<string> &token, bool &run)
+{
+/*	if(token.size())//check if omit next command based on connector and exit status
+	{
+		if(token[0].compare("&&") || token[0].compare("||") || token[0].compare(";"))
+		{
+			wait(0);
+			if((status == 0 && token[0].compare("||") == 0) || (status != 0 && token[0].compare("&&") == 0))
+			{
+				token.erase(token.begin());//erase connector
+			}
+			else
+				if(token.size())
+					token.erase(token.begin()); // erase connect or erase next command if previous if went off
+		}
+	}
+	else
+		return; // empty list
+*/
+	if(token.size())
+	{
+		//find next connector!
+		int i = nextConnector(token);
+		if( i < token.size() ){
+			if( !token[i].compare("|") )
+				doPipe(token, run,i);
+			else if( !token[i].compare("||") )
+				doOr(token, run,i);
+			else if( !token[i].compare("&&") )
+				doAnd(token,run,i);
+			else if( !token[i].compare(";") )
+				doSemiColon(token, run, i);
+			else redirectAndRun(token,run,i);
+		}else
+			redirectAndRun(token,run,i);
+		
+	}
+}
+
+void doPipe(vector<string> &token, bool &run, int index)
+{
+	int fd[2];
+	pipe(fd);
+
+	int childPID = fork();
+	if(childPID == -1)
+	{
+		perror("fork error");
+		run = false;
+	}
+	if(childPID != 0) //parent process
+	{
+		dup2(fd[1],1);
+		close(fd[0]);
+		close(fd[1]);
+		redirectAndRun(token,run,index);
+		int status;
+		//waitpid(childPID,&status,0);
+	} else //child process
+	{
+		dup2(fd[0],0);
+		close(fd[0]);
+		close(fd[1]);
+		for(int i = 0; i <= index; ++i)
+			token.erase(token.begin());
+		commands(token,run);
+	}
+}
+
+void doOr(vector<string> & token, bool &run, int index)
+{
+	int status = redirectAndRun(token,run,index);
+
+	if(status == 0)
+		return;	
+	
+	for(int i = 0; i <= index; ++i)
+		token.erase(token.begin());
+	commands(token,run);
+}
+
+void doAnd(vector<string> &token, bool &run, int index)
+{
+	int status = redirectAndRun(token,run,index);
+
+	if(status != 0)
+		return;	
+	
+	for(int i = 0; i <= index; ++i)
+		token.erase(token.begin());
+	commands(token,run);
+}
+
+void doSemiColon(vector<string> &token, bool &run, int index)
+{
+	int status = redirectAndRun(token,run,index);
+
+	for(int i = 0; i <= index; ++i)
+		token.erase(token.begin());
+	commands(token,run);
+}
+
+void doLeft1(vector<string> &token, bool &run, int index)
+{
+	int i = 0,fileIn;
+	for(;i < token.size(); ++i)
+		if(token[i].find("<") != string::npos)
+			break;
+
+	if(i < index)
+	{
+		if(!token[i].compare("<"))
+			if(-1 == (fileIn = open(token[i+1].c_str(),O_RDONLY , 0644)))
+    		{
+        		perror("error opening output file");
+       			run = false;
+       			return;
+    		}
+
+		dup2(fileIn,0);
+	}
+}
+
+void doRight(vector<string> &token, bool &run, int index)
+{
+	int i = 0,in = 1, out = 0,fileOut;
+	bool boolFD = true;
+	for(;i < token.size(); ++i)
+		if(token[i].find(">") != string::npos)
+			break;
+	if(i < index)
+	{
+		if(isdigit(token[i][0])){
+			char buf[2] = {token[i][0],0};
+			out = atoi(buf);
+		}
+		if(token[i].size() > 2)
+			if(isdigit(token[i][(token[i].size()-1)]) && token[i][token.size()-2] == '&')
+			{
+				char buf[2] = {token[i][token[i].size()-1],0};
+				in = atoi(buf);	
+				boolFD = false;
+			}
+		
+		if(boolFD)
+		{
+			if(token[i].find(">>") != string::npos)
+			{
+					if(-1 == (fileOut = open(token[i+1].c_str(), O_WRONLY | O_APPEND |  O_CREAT, 0644)))
+    				{
+        				perror("error opening output file");
+                		run = false;
+                		return;
+    				}
+			}else{
+				if(-1 == (fileOut = open(token[i+1].c_str(), O_WRONLY |  O_CREAT, 0644)))
+    				{
+        				perror("error opening output file");
+						run = false;
+        	        	return;
+    				}
+			}
+			dup2(fileOut,in);
+		}else{
+			dup2(in,out);
+		}
+			
+	}
+}
+
+int doLeft3(vector<string> &token, bool &run, int index)
+{
+	int i = 0;
+	for(;i < token.size(); ++i)
+		if(!token[i].compare("<<<"))
+			return i;
+	return i;
+}
+
+int redirectAndRun(vector<string> &token, bool &run, int index)
+{
+	//do redirection!
+	doLeft1(token,run,index);
+	int l3 = doLeft3(token,run,index);
+	doRight(token,run,index);
+	//...
+
+	int fd[2] = {0,0};
+	if(l3 < index)
+	{
+		pipe(fd);
+	}
+	//do execute command!
+	int childPID = fork();
+	if(childPID == -1)
+	{
+		perror("fork error");
+		run = false;
+	}
+	if(childPID != 0)
+	{
+        if(l3 < index){
+			close(fd[0]);
+			int outSize = 0;
+    	    if((outSize = write(fd[1],token[l3+1].c_str(),token[l3+1].size())) == -1)
+        	{
+				perror("write error");
+				run = false;
+				return -1;
+			}
+			close(fd[1]);
+		}
+		int status = 0;
+		waitpid(childPID,&status,0);
+		return status;
+	} else
+	{
+		if(l3 < index) {
+			dup2(fd[0],0);
+			close(fd[0]);
+			close(fd[1]);
+		}
+		
+		int i(0);
+		itemCount(token[0]);
+		char *array[numArgs+1];
+		for(int k = 0; k < numArgs; ++k){
+			array[k] = new char[256];
+			memset(array[k],0,256);
+		}
+		array[numArgs] = 0;		
+
+		string test;
+		stringstream ss;
+		ss << token[0];
+		while(ss >> test)// fill argv
+		{
+			strcpy(array[i],test.c_str());
+			i++;
+		}
+
+		if (execvp(array[0],array) != 0){
+			perror(array[0]);
+			for(int k = 0; k < numArgs+1; ++k)
+				delete[] array[k];
+			exit(1);
+		}
+	}
+}
+
 void parse(string s, bool &run)
 {
 	queue<string> list;
+
 	list = split(s);//fuction creates substrings of commands and separators
-	int status(0);
+
+	vector<string> vList;
 	while(!list.empty())
+	{
+		vList.push_back(list.front());
+		list.pop();
+	}
+
+	int status(0);
+	while(!vList.empty())
 	{
 		stringstream ss,ss2;
 		string test;
-		ss << list.front();
-		ss2 << list.front();
-		itemCount(list.front());
-		test = list.front();
-		list.pop();
+		ss << vList[0];
+		ss2 << vList[0];
+		
+		itemCount(vList[0]);
+		
+		test = vList[0];
+		vList.erase(vList.begin());
+		string next;
+
+		if(!vList.empty())
+			next = vList[0];
+
+
+		if(next.compare("|") == 0 || next.find(">") != string::npos || next.find("<") != string::npos)	
+			cout << "FOUND CHAIN\n"; //function to extract chain of redirectinons
+		
+		//process chain
+
+
+/*
 		// check if we have commands or connectors and depending on the connector check the previous exit status
 		if(test == ";")
 			continue;//continue means run the command
@@ -102,7 +462,7 @@ void parse(string s, bool &run)
 		{
 			if(status == 0) // previous command succeeded (TRUE || command)
 			{
-				list.pop();
+				vList.erase(0);
 				continue;
 			}
 			else
@@ -112,13 +472,13 @@ void parse(string s, bool &run)
 		{
 			if(status != 0)// previous command failed (FALSE && command)
 			{
-				list.pop();
+				vList.erase(0);
 				continue;
 			}
 			else
 				continue;// previous command succeeded (TRUE && command)
 		}
-		else 
+		else //map of signals and commands
 		{
 			ss2 >> test;	
 			if(test.compare("exit") == 0){
@@ -126,16 +486,19 @@ void parse(string s, bool &run)
 				break;	
 			}
 		}
+
 		init();
 		int i(0);
+		
 		while(ss >> test)// fill the char** normally referred to as args to pass to execvp
 		{
 			strcpy(array[i],test.c_str());
 			i++;
 		}
+		
 		status = sysCalls();
 		nukem();
-	}
+*/	}
 }
 
 void init()// initializes char*[] or char ** array
@@ -198,7 +561,8 @@ queue<string> split(string s)//check the connectors to create substrings
 			{
 				if(next == '&')
 				{
-					list.push(s.substr(sPos,i-sPos));
+					if(i-sPos)
+						list.push(s.substr(sPos,i-sPos));
 					list.push("&&");
 					sPos = (i+2);
 					i++;
@@ -209,14 +573,16 @@ queue<string> split(string s)//check the connectors to create substrings
 			{
 				if(next == '|')
 				{
-					list.push(s.substr(sPos,i-sPos));
+					if(i-sPos)
+						list.push(s.substr(sPos,i-sPos));
 					list.push("||");
 					sPos = (i+2);
 					i++;
 				}
 				else
 				{
-					list.push(s.substr(sPos,i-sPos));
+					if(i-sPos)
+						list.push(s.substr(sPos,i-sPos));
 					list.push("|");
 					sPos = (i+1);
 
@@ -234,24 +600,66 @@ queue<string> split(string s)//check the connectors to create substrings
 			{
 				if(next == '<' && next2 == '<') // append "<"
 				{
-					list.push(s.substr(sPos,i-sPos));
+					if(i-sPos)
+						list.push(s.substr(sPos,i-sPos));
 					list.push("<<<");
 					sPos = (i+3);
 					i+=2;
 				}
 				else // append "<"
 				{
-					list.push(s.substr(sPos,i-sPos));
+					if(i-sPos)
+						list.push(s.substr(sPos,i-sPos));
 					list.push("<");
 					sPos = (i+1);
 				}
 			}
-		case '>'://check check fd range 0-255****************************************************************************************************
+			break;
+		case '>':// #> || #>&# || #>> || >> || > 
 			{
-				// if #>
-				// if #>$#
-				// if >>
-				// if >
+				if(isdigit(previous)) // #> || #>&#
+				{
+					if(next == '&' && isdigit(next2)) // #>&#
+					{
+						if((i-1)-sPos)
+							list.push(s.substr(sPos,(i-1)-sPos));
+						list.push(s.substr(i-1,4));
+						sPos = (i+3);
+						i+=2;
+					}
+					else if(next == '>')// #>>
+					{
+						if((i-1)-sPos)
+							list.push(s.substr(sPos,(i-1)-sPos));
+						list.push(s.substr(i-1,3));
+						sPos = (i+2);
+						i++;
+					}
+					else // #>
+					{
+						if((i-1)-sPos)
+							list.push(s.substr(sPos,(i-1)-sPos));
+						list.push(s.substr((i-1),2));
+						sPos = (i+1);
+					}
+					break;
+				}
+				else if(next == '>') // >>
+				{
+					if(i-sPos)
+						list.push(s.substr(sPos,i-sPos));
+					list.push(">>");
+					sPos = (i+2);
+					i++;
+					break;
+				}
+				else // >
+				{
+					if(i-sPos)
+						list.push(s.substr(sPos,i-sPos));
+					list.push(">");
+					sPos = (i+1);
+				}
 			}
 		default:;
 		}
@@ -277,4 +685,13 @@ void itemCount(string s)//set glocal variables for array creation
 	}
 	numArgs = worker.size();
 	numLen = size;
+}
+
+int nextConnector(vector<string> &token)
+{
+	int i = 0;
+	for(;i<token.size();++i)
+		if( !token[i].compare("|") || !token[i].compare("&&") || !token[i].compare("||") || !token[i].compare(";"))
+			return i;
+	return i;
 }
